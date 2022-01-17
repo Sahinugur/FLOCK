08afb98ed72e23f530afcbb780e49d65351781e0
Evgira Budishcheva <bevgira@gmail.com>
2022-01-17 10:49:10 +0100

    added postRouter, created model folder

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ const app = express();
 
 const http = require("http");
 
+/** Include routers */
+const postRouter = require("./router/postRouter");
+
+/** Middleware */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/** Routers */
+app.use("/post", postRouter);
+
 const server = http.createServer(app);
 
 const PORT = process.env.port || 5000;

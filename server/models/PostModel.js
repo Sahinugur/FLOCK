c84ce9651b63c23112682b6821602ca859e41139
Pavel Penchev <p.penchev90@gmail.com>
2022-01-18 11:46:12 +0100

    generating feature branch : Post

--- server/models/PostModel.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-// const { Schema } = mongoose;
-
-// const PostModel = new Schema({
-//   title: { type: String },
-//   author: { type: String },
-// });
-
-// module.exports = mongoose.model("Post", PostModel);
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -13,3 +13,34 @@ router.post("/createPost", createPost);
 // router.route("/createPost").post(createPost);
 
 module.exports = router;
+
+const multer = require("multer");
+const upload = require("../helpers/multer");
+const videoUpload = require("../helpers/multer");
+const postSchema = require("../models/Post");
+
+router.post("/single", upload.single("image"), (req, res, next) => {
+  try {
+    console.log(req.file, req.body);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  "/uploadVideo",
+  videoUpload.single("video"),
+  (req, res, next) => {
+    res.send(req.file);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+// router.get('/getLatest', async (req, res) => {
+//     const getImage = await postSchema.findOne().sort({ _id: -1 });
+//     res.json(getImage.imageUrl);
+// });
+
+module.exports = router;

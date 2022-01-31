803966fe41ecd117cc0dd8ff798814321554de6c
PatrickJaskiela <pemtick@web.de>
2022-01-31 11:51:10 +0100

    mediaQueryAdded

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className="grid-container">´
       <NavBar />
-      <h1 className = "head">this is the Home page</h1>
+      {/* <h1 className = "head">this is the Home page</h1> */}
       {state.user.source !== "github" ? (
         <h2>{state.user.email}</h2>
       ) : (

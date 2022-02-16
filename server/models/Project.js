dbf9bfaca47288e9079f51a8b2e0e16cf2d30977
Evgira Budishcheva <bevgira@gmail.com>
2022-02-16 14:26:32 +0100

    updated projectController

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,23 +2,34 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const projectSchema = new Schema({
-    title: 
-        {type: String, minlength: 3, maxlength: 255, required: true},
-    founder: 
-        { type: Schema.Types.ObjectId, ref: "User" },
-    size_of_project:
-        [{type: String}, {enum: ["small", "medium", "large"]}],
-    participants: 
-        String,
-    type_of_project: 
-        [{ type: String }, { enum: ["Static Web Apps", "Dynamic Web Apps", "Single Page Apps", "Multiple Page Apps", "Animated Web Apps", "Content Management System", "E-commerce Apps", "Portal Web Apps", "Progressive Web Apps" ] }],
-    technologies: 
-        String,
-    stage_of_project: 
-        [{type: String}, {enum: ["first steps", "in construction", "done"]}],
-    add_link: String,
-    few_words: 
-        {type: String, minlength: 3, maxlength: 255}
+  title: { type: String, minlength: 3, maxlength: 255, required: true },
+  founder: { type: Schema.Types.ObjectId, ref: "User" },
+  size_of_project: [{ type: String }, { enum: ["small", "medium", "large"] }],
+  participants: String,
+  add_link: { type: String },
+  type_of_project: [
+    { type: String },
+    {
+      enum: [
+        "Static Web Apps",
+        "Dynamic Web Apps",
+        "Single Page Apps",
+        "Multiple Page Apps",
+        "Animated Web Apps",
+        "Content Management System",
+        "E-commerce Apps",
+        "Portal Web Apps",
+        "Progressive Web Apps",
+      ],
+    },
+  ],
+  technologies: String,
+  stage_of_project: [
+    { type: String },
+    { enum: ["first steps", "in construction", "done"] },
+  ],
+
+  few_words: { type: String, minlength: 3, maxlength: 255 },
 });
 
 module.exports = mongoose.model("Project", projectSchema);

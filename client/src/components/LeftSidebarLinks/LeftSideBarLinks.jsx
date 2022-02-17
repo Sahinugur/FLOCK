1b86a4c8f53d60cd423c647e00fd8e74d23b9110
PatrickJaskiela <pemtick@web.de>
2022-02-17 15:50:21 +0100

    testCommit

--- client/src/components/LeftSidebarLinks/LeftSideBarLinks.jsx
+++ client/src/components/LeftSidebarLinks/LeftSideBarLinks.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./leftsidebar.css";
-import "./LeftSideBar.css";
+
 
 export default function LeftSideBarLinks() {
   const navigate = useNavigate();

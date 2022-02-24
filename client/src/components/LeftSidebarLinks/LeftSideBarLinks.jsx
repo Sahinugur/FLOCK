229816be796186c86e2a4629e75a8acf1b8bdda0
PatrickJaskiela <pemtick@web.de>
2022-02-24 11:26:56 +0100

    weird

--- client/src/components/LeftSidebarLinks/LeftSideBarLinks.jsx
+++ client/src/components/LeftSidebarLinks/LeftSideBarLinks.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./leftsidebar.css";
+import "./LeftSideBar.css";
 
 export default function LeftSideBarLinks() {
   const navigate = useNavigate();
